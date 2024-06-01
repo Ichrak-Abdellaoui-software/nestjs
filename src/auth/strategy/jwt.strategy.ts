7735a19b10856d48877930af4ec4bcaf49379f1e
Rim Bziouich <86674923+RimaBzch@users.noreply.github.com>
2024-06-01 13:56:30 +0100

    Update jwt.strategy.ts

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -14,8 +14,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    console.log('JWT Validate Payload:', payload);
+    //console.log('JWT Validate Payload:', payload);
     if (!payload || !payload.email) {
+      //if (!payload || !payload.sub || !payload.email)
       throw new UnauthorizedException('Invalid token');
     }
     //return { email: payload.email, sub: payload.sub };
